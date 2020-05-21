cae52b35ea61f89cd867fdc1cb6ebfd24e91f5f4
StopTouchingMeElmo <mezhdumirie@gmail.com>
2020-05-21 14:59:19 +0300

    fetch

--- VanillaJS/14_ServerRequests_Fetch.js
+++ VanillaJS/14_ServerRequests_Fetch.js
@@ -11,7 +11,8 @@ const requestURL = 'https://jsonplaceholder.typicode.com/users' //[0]
 // Метод fetch возвращает Promise
 // По умолчанию метод fetch будет выполнять запрос GET
 function sendRequest(method, url, body) {
-    return fetch(url).then(response => response.json())
+    return fetch(url).then(response => response.json()) // response.json() парсим json и возвращаем промис
+    //(10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
 }
 
 sendRequest('GET', requestURL)
@@ -22,7 +23,35 @@ const user = {
     name: 'Steve',
     age: 30
 }
+//======================================================================
 
-/* sendRequest('POST', requestURL, user)
-.then(data => console.log(data))
-.catch(err => console.log(err)); */
+//POST
+function sendRequest2(method, url, body) {
+    return fetch(url, { //вторым аргументом передаем объект конфигурации тела
+        method: method,
+        body: JSON.stringify(body),
+        headers: headers
+    }).then(response => {
+        if (response.ok) { // или response.status < 400
+            return response.json()
+        }
+        return response.json() //обрабатываем и выбрасываем ошибку
+            .then(error => {
+                const e = new Error('Sorry, smthn gone wrong')
+                e.data = error
+                throw e
+            })
+    })
+}
+
+const headers = {
+    'Content-Type': 'application/json'
+}
+
+sendRequest2('POST', requestURL, user)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+//Res:
+//{name: "Steve", age: 30, id: 11}
+
+//Библиотеки по типу JQuery построены на этих двух методах (XHR и Fetch), которые являются нативными для браузеров
