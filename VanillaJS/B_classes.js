81bcea77f4b6656ea8b38d0bdb024ac6b48dcb79
StopTouchingMeElmo <mezhdumirie@gmail.com>
2020-07-20 03:18:32 +0300

    arrows this in classes/objects

--- VanillaJS/B_classes.js
+++ VanillaJS/B_classes.js
@@ -9,7 +9,8 @@
 // Хоть JS построен не на классах, а на прототипах,
 // программисты пришли к такому паттерну объектно ориентированного кода:
 
-function Animal(name, voice) { // конструктор
+function Animal(name, voice) {
+  // конструктор
   this.name = name;
   this.voice = voice;
 }
@@ -34,7 +35,8 @@ dog.say();
 // duck -> Bird.prototype -> AnimalClass.prototype -> Object.prototype -> null
 
 class AnimalClass {
-  constructor(name, voice) { // конструктор
+  constructor(name, voice) {
+    // конструктор
     this.name = name;
     this.voice = voice;
   }
@@ -65,7 +67,7 @@ duck.say();
 // При добавлении своего конструктора в класс наследник необходимо в нем вызвать конструктор супер класса, через super.
 // При этом вызвать super() необходимо до первого использования ключевого слова this.
 
-// Ключевое слово super дает доступ ни только к супер конструктору, но и к любому методу в супер классе.
+// Ключевое слово super дает доступ не только к супер конструктору, но и к любому методу в супер классе.
 
 // Если необходимо, мы пожем полностью переопределить функцию say() для класса Bird
 // Для этого мы просто создаем в теле класса Bird функцию с таким же именем.
@@ -76,3 +78,33 @@ duck.say();
     super(name, voice);
     this.say();
  */
+//==============================================================================================================
+
+// Варианты создания функции внутри объекта (не в прототипе):
+// 1. Классический способ:
+class SomeClass {
+  constructor(name, voice) {
+    // конструктор
+    this.name = name;
+    this.voice = voice;
+    // просто передаем функцию в объект через конструктор
+    this.fnInsideObj = () => {
+      console.log("i'm inside object");
+    };
+  }
+  say() {
+    console.log("Animal", this.name, "says:", this.voice); // прототип
+  }
+}
+
+// 2. Proposal Class Fields, еще не вошедший в стандарт.
+// Просто пишем функцию в теле класса.
+class SomeClass3 extends SomeClass {
+  fnInsideObj2 = () => {
+    console.log("i'm inside object too");
+  };
+}
+
+const inst = new SomeClass3();
+inst.fnInsideObj2();
+//i'm inside object too
